96b64e3d46d387c225a129cd269cc998a59aa34c
Vladimir Mashyanov <88646034+VL1507@users.noreply.github.com>
2023-10-20 22:43:17 +1000

    E6 Собственное значение матрицы

---
+++ E6 Собственное значение матрицы/solve.c
@@ -0,0 +1,23 @@
+// https://lms2.sseu.ru/courses/eresmat/course1/razd8z1/par8_18z1.htm
+// для понимания задачи и решения
+
+#include <stdio.h>
+
+int main() {
+    double a, b, c, l;
+    char d;
+    scanf("%lf %lf %lf %c %lf", &a, &b, &c, &d, &l);
+
+    double numerator = a * l + c * b - l * l;
+    double denominator = a - l;
+
+    if (numerator == 0 && denominator == 0) {
+        printf("INF");
+    } else if (denominator == 0) {
+        printf("NO");
+    } else {
+        printf("YES\n%.4lf", numerator / denominator);
+    }
+    
+    return 0;
+}
