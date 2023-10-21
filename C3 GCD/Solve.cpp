c1025a08d503dd886c8a3b85aa9068b496872153
eMD0gG <147574275+eMD0gG@users.noreply.github.com>
2023-10-22 01:52:01 +1000

    Solve.cpp

---
+++ C3 GCD/Solve.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int NOD(int a, int b) {
+    while (a != 0 and b != 0) {
+        if (a > b) {
+            a = a % b;
+        } else {
+            b = b % a;
+        }
+    }
+    return (a+b);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << NOD(a, b) << ' ' << NOD(a, c) << ' ' << NOD(b, c);
+}
+
+
+
