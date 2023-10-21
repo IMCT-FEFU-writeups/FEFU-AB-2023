b1bf72e33821a6189eceaafc1e1e1b96038db929
eMD0gG <147574275+eMD0gG@users.noreply.github.com>
2023-10-21 22:18:11 +1000

    solve.cpp

--- AD Сколько селёдок?/solve.cpp
+++ AD Сколько селёдок?/solve.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <string>
-#include <bitset>
+#include <cmath>
 
 using namespace std;
 
-string bin(unsigned long long int n) {
-    bitset<24> bin(n);
-    return bin.to_string();
-}
-
 int main() {
-    unsigned long long int n;
-    string bn;
-    char colour;
-    cin >> n >> colour;
-    bn = bin(n);
-    if (colour == 'b') {
-        cout << stoi(bn.substr(16, 8), 0, 2);
+    long double wm, a = 0;
+    long long int n;
+    cin >> wm;
+    n = (sqrt(8 * wm + 1) - 1) / 2;
+    if (sqrt(wm * 8 + 1) == int(sqrt(wm * 8 + 1))) {
+        cout << 1;
         return 0;
     }
-    if (colour == 'g'){
-        bn=bn.substr(8, 8)+"00000000";
-        cout << stoll(bn, 0, 2);
-        return 0;
+    for (long long int i = n; i > 0; i--) {
+        a = wm - (i * (i + 1) / 2);
+        if (sqrt(a * 8 + 1) == int(sqrt(a * 8 + 1))) {
+            cout << 2;
+            return 0;
+        }
     }
-    if (colour == 'r'){
-        bn=bn.substr(0, 8)+"0000000000000000";
-        cout << stoull(bn, 0, 2);
-        return 0;
-    }
+    cout << 3;
 }
