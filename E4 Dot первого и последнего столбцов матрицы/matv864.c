87e127ba71f5ade6ca74be5187a4314e992c5e4b
matv864 <91239322+matv864@users.noreply.github.com>
2023-10-21 23:58:19 +1000

    Update matv864.c

--- E4 Dot первого и последнего столбцов матрицы/matv864.c
+++ E4 Dot первого и последнего столбцов матрицы/matv864.c
@@ -4,6 +4,8 @@
 считывай матрицу
 проходи как надо
 3 тест - это m = 1
+
+решение без матрицы дальше
 */
 #include <stdio.h>
 int main(){
@@ -23,28 +25,7 @@ int main(){
     
     return 0;
 }
-/*
-дополнительное решение без матрицы
 
-*/
-
-#include <stdio.h>
-int main(){
-    long long int n, m, first, last, trash, res = 0;
-    scanf("%lld %lld", &n, &m);
-    
-    for (int i = 0;i < n;i++){
-        scanf("%lld", &first);
-        last = first; // на случай, когда m = 1
-        for (int j = 0;j < m-1;j++){
-            scanf("%lld", &last);
-        }
-        res += (first * last);
-    }
-    printf("%lld", res);
-    
-    return 0;
-}
 
 
 
