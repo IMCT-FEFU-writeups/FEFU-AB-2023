d5b6ada8dd227c2023fa5944f602ff8fa7bfe75a
davikch <139548630+davikch@users.noreply.github.com>
2023-10-24 15:27:31 +1000

    Add files via upload
    style

---
+++ B1 Двоичный порядок float/b1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    unsigned int a;
+    int output = -127;
+	scanf("%f", &a);
+    /* Float doesn't allow bitwise operations
+    Input is decoded like a float, but the raw bits are put 
+    inside unsinged int variable */
+
+    if (a == 0) { printf("%d", 0); return 0; }  // Zero
+
+    if (a >> 23 == 0 || a >> 23 == 256) { printf("%d", -126); return 0; }
+    /* Denormized number: have to output -126 but the reason
+    is not clear to me yet */
+
+    for (int i = 0; i <= 7; i++) {
+        if (a & 1 << 30 >> i) { output += pow(2, 7 - i); }
+    }
+	printf("%d", output);
+    return 0;
+}
