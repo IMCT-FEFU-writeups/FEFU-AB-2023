0bc389e159d82ecccd8b8b55dd4e5d2da531d40b
davikch <139548630+davikch@users.noreply.github.com>
2023-10-24 15:29:34 +1000

    Add files via upload

--- B1 Двоичный порядок float/davikch.c
+++ B1 Двоичный порядок float/davikch.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{	
-	unsigned int a;
-  int output = -127;
-	scanf("%f", &a); //Нужны только биты; биты флоата записываются в интеджер намеренно, флоат не позволяет побитовые операции
-    if (a == 0){ printf("%d", 0); return 0; } //Ноль
-    if (a >> 23 == 0 || a >> 23 == 256){ printf("%d", -126); return 0; } //Денормализованное число, надо вывести -126; я что-то подозреваю, но полное понимание причины пока что за пределами моего сознания
-    for (int i = 0; i <= 7; i++){
+int main() {
+    unsigned int a;
+    int output = -127;
+	scanf("%f", &a);
+    /* Float doesn't allow bitwise operations
+    Input is decoded like a float, but the raw bits are put 
+    inside unsinged int variable */
+
+    if (a == 0) { printf("%d", 0); return 0; }  // Zero
+
+    if (a >> 23 == 0 || a >> 23 == 256) { printf("%d", -126); return 0; }
+    /* Denormized number: have to output -126 but the reason
+    is not clear to me yet */
+
+    for (int i = 0; i <= 7; i++) {
         if (a & 1 << 30 >> i) { output += pow(2, 7 - i); }
     }
 	printf("%d", output);
-  return 0;
+    return 0;
 }
